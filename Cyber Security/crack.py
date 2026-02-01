b88c629a2695c7c3a40ef5ea2485d37cd3c5bb32
lonepu <mgpumgpu.1997@gmail.com>
2026-02-01 11:30:45 +0630

    newupdate

---
+++ Cyber Security/crack.py
@@ -0,0 +1,8 @@
+from string import digits
+
+for i in digits:
+    for j in digits:
+        for k in digits:
+            for l in digits:
+                print(i, j, k, l)
+#4 digit Password is not very secure

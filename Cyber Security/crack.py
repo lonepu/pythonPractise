fae044f9f225c10587eeea8d2be0f91d56e8968f
lonepu <mgpumgpu.1997@gmail.com>
2026-02-28 14:28:11 +0630

    updated

--- Cyber Security/crack.py
+++ Cyber Security/crack.py
@@ -1,19 +1,19 @@
-from string import digits
+# from string import digits
 
-for i in digits:
-    for j in digits:
-        for k in digits:
-            for l in digits:
-                print(i, j, k, l)
+# for i in digits:
+#     for j in digits:
+#         for k in digits:
+#             for l in digits:
+#                 print(i, j, k, l)
 #4 digit Password is not very secure
 #
-# from string import ascii_letters
-#
-# for i in ascii_letters:
-#     for j in ascii_letters:
-#         for k in ascii_letters:
-#             for l in ascii_letters:
-#                 print(i, j, k, l)
+from string import ascii_letters
+
+for i in ascii_letters:
+    for j in ascii_letters:
+        for k in ascii_letters:
+            for l in ascii_letters:
+                print(i, j, k, l)
 #
 # from string import ascii_letters, digits, punctuation
 #
---
+++ ifcondition.py
@@ -0,0 +1,28 @@
+# print("The Day are \n 1 for Monday\n 2 for Tuesday\n 3 for Wednesday\n 4 for Thursday\n 5 for Friday")
+# day = int(input("Enter Your Number: "))
+# if day == 1: print("Monday")
+# elif day == 2: print("Tuesday")
+# elif day == 3: print("Wednesday")
+# elif day == 4: print("Thursday")
+# elif day == 5: print("Friday")
+# elif day == 6: print("Thursday")
+# elif day == 7: print("Friday")
+
+# a = 50
+# b = 10
+# if a == b:
+#     print("1")
+# elif a > b:
+#     print("2")
+# else:
+#     print("3")
+
+a = 200
+b = 33
+if b > a:
+    print("b is greater than a")
+elif a == b:
+    print("a and b are equal")
+else:
+    print("b and a are equal")
+
